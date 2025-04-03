bac30c3069c6b296f7d8a9f480a5c45fec58319b
Dave <dave@davebeauchemin.com>
2025-04-03 07:40:33 -0400

    Add product page

--- app/product/page.tsx
+++ app/product/page.tsx
@@ -1,7 +1,5 @@
 import { getProduct } from "@/lib/surecart";
 
 export default async function ProductPage() {
-  const product = await getProduct("e7dad057-cc0e-4f0c-8a8f-226a0894e855");
-  console.log(product);
   return <div>Product</div>;
 }
