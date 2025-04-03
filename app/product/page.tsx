5c8826522334b0089a09739a63b5e59bdf0a809b
Dave <dave@macbookpro.home>
2025-04-03 07:38:23 -0400

    Add product page

--- app/product/page.tsx
+++ app/product/page.tsx
@@ -1,3 +1,7 @@
-export default function ProductPage() {
+import { getProduct } from "@/lib/surecart";
+
+export default async function ProductPage() {
+  const product = await getProduct("e7dad057-cc0e-4f0c-8a8f-226a0894e855");
+  console.log(product);
   return <div>Product</div>;
 }
