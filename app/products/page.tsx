6ebed85eefa50170b38de56a64ddcf2bd5e6dc9e
Dave <dave@macbookpro.home>
2025-04-03 07:27:53 -0400

    SureCart Initialization finished

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,3 +1,9 @@
-export default function ProductsPage() {
+import { fetchFromSureCart, getProducts } from "@/lib/surecart";
+
+export default async function ProductsPage() {
+  const products = await getProducts();
+
+  console.log(products);
+
   return <div>Products</div>;
 }
